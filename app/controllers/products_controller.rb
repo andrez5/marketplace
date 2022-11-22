491c956437d08dac2d311e81ac4837e54b01df6e
Michel Moutinho <michelmoutinhofr1@gmail.com>
2022-11-22 14:28:54 -0300

    routes and product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
   def new
   end
+
+  def destroy
+    @product = product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, status: :see_other
+  end
+
 end
