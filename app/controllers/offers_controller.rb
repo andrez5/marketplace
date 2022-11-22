9656ab8b64fa48da6daf893a53cb4cc3fbf97ad4
Sergio Kummer <sergiokummer96@gmail.com>
2022-11-22 12:23:41 -0300

    dalhe

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,4 +3,8 @@ class OffersController < ApplicationController
   def index
     @offers = Offer.all
   end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "offers", to: 'offers#index'
+  get "offers/:id/edit", to: 'offers#edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
