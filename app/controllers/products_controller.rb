7b275bbd81764a901e8aea3c86c693005ba466f1
Felipe Andrez Gonzalez <felipeandrez@gmail.com>
2022-11-22 12:31:20 -0500

    fiz a rota, controller do product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,18 @@
 class ProductsController < ApplicationController
   def new
+    @products = Product.all
   end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    # redirect_to product_path(@product)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :info, :photo, :price, :available, :user_id)
+  end
+
 end
