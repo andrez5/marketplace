3f398579169b5d48997bbd66792434f349cec413
Felipe Andrez Gonzalez <felipeandrez@gmail.com>
2022-11-22 10:13:48 -0500

    acabei a view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,7 +5,7 @@ class OffersController < ApplicationController
   end
 
   def show
-
+    @offer = Offer.find(params[:id])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "offers", to: 'offers#index'
+
+
+  get "offers/new", to: "offers/#new"
+  get "offers/:id", to: "offers/show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
